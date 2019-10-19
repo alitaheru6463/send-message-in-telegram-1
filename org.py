2b8d0a074cb6be0f57a9c4f38e2cca82e63a2060
ar-man-org <56646518+ar-man-org@users.noreply.github.com>
2019-10-19 09:16:29 -0700

    Update org.py

--- org.py
+++ org.py
@@ -3,15 +3,16 @@ wq='\t*******************************************************\n\n\n \t         t
 print(wq)
 id=input("\n\nid ? ? ?")
 message=input("message ? ? ?")
+m=message.replace(" ","+")
 pass1='9323'
 q=4
 w=0
 n=input("what is password? ? ?")
 if n == pass1:
     
-    site = "http://persian-cyber.xyz/send-message/AR-MAN.php?message="+message+"&id="+id+"&pass=@Persian_Cyer_Org"
+    site = "http://persian-cyber.xyz/send-message/AR-MAN.php?message="+m+"&id="+id+"&pass=@Persian_Cyer_Org"
     mmd=builtwith.parse(site)
-    print("ok")
+    print("OK ! !  done successfully")
 else :
     print("error password")
     
